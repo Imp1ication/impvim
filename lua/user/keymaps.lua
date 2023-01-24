f89ddac491720e0eec6670625c3243e8d49e1a19
1mplication <1mplicationxwzi@gmail.com>
2023-01-25 01:45:23 +0800

    upload files

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,69 @@
+-- Shorten function name --
+local keymap = vim.api.nvim_set_keymap
+
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+-- Remap <Space> as leader key --
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Page up and down
+keymap("n", "<C-u>", "15kzz", opts)
+keymap("n", "<C-d>", "15jzz", opts)
+
+-- Keep cursor at the middle after search jumping
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+
+-- Move in warpline 
+keymap("n", "j", "gj", opts)
+keymap("n", "k", "gk", opts)
+keymap("n", "gj", "j", opts)
+keymap("n", "gk", "k", opts)
+
+-- Delete into void
+keymap("n", "<leader>d", "\"_d", opts)
+
+-- Replace all
+keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
+
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows 
+keymap("n", "<C-Up>", ":resize -2<Cr>", opts)
+keymap("n", "<C-Down>", ":resize +2<Cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<Cr>", opts)
+keymap("n", "<C- Right>", ":vertical resize -2<Cr>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<Cr>", opts)
+keymap("n", "<S-h>", ":bprevious<Cr>", opts)
+
+-- Add empty line without get into Insert mode 
+keymap("n", "<Cr>", "o<Esc>k", opts)
+
+
+-- Insert -- 
+-- Press jj to exit insert mode 
+keymap("i", "jj", "<Esc>", opts)
+
+
+-- Visual --
+-- Stay in indent mode 
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Keep paste buffer after paste
+keymap("v", "p", "\"_dP", opts)
+
+-- Move text up and down
+keymap("v", "J", ":m '>+1<Cr>gv=gv", opts)
+keymap("v", "K", ":m '<-2<Cr>gv=gv", opts)
+
---
+++ lua/user/options.lua
@@ -0,0 +1,57 @@
+local options = {
+    -- system related --
+    fileencoding = "utf-8",     -- encoding
+    clipboard = "unnamedplus",  -- allows nvim to access the OS clipboard
+    mouse = "ic",               -- enabled mouse in Insert mode and Command-line mode
+    timeoutlen = 300,
+    updatetime = 300,
+    completeopt = { "menuone", "noselect" },    -- auto cmp but don't select 
+    conceallevel = 0,           -- make `` visible in markdown files
+
+    -- backup --
+    backup = false,
+    swapfile = false,
+    undofile = true,            -- enable persistent undo
+
+    -- line number and line column --
+    number = true,
+    relativenumber = true,
+    numberwidth = 4,            -- set number column width to 4 
+    signcolumn = "yes",
+    
+    -- tab and indentation --
+    tabstop = 4,
+    softtabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,           -- expand tab to spaces
+    autoindent = true,          -- copy indent from current line when starting a new line 
+    smartindent = true,         -- make above smarter
+    
+    -- wrap and scrolloff
+    wrap = false,               -- disable line wrapping
+    scrolloff = 4,              -- minimal number of screen lines to keep above and below the cursor
+    sidescrolloff = 8,
+
+    -- search --
+    hlsearch = false,
+    incsearch = true,           -- search while typing a search command 
+    smartcase = true,
+    -- ignorecase = true,       -- if you don't like smartcase, you can just instead it with this option
+
+    -- appearance --
+    showtabline = 2,            -- always show tabs
+    -- cursorline = true,
+    cmdheight = 2,  
+    pumheight = 10,             -- pop up menu heaght
+    showmode = false,           -- with powerline, we don't need to show things like -- INSERT -- anymore
+    splitbelow = true,          -- split horizontal widow to the below
+    splitright = true,          -- split vertical widow to the right
+    termguicolors = true,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.opt.iskeyword:append "-"    -- consider string-string as whole word
+vim.g.ruby_host_prog = "/usr/local/bin/neovim-ruby-host"
