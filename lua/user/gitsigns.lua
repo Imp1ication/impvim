892cb52e914a1ee3c18c88909417aa7debd6ae7c
1mplication <1mplicationxwzi@gmail.com>
2023-02-19 20:35:32 +0800

    update gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -16,20 +16,23 @@ gitsigns.setup({
 	numhl = false,
 	linehl = false,
 	word_diff = false,
+	sign_priority = 6,
+
 	watch_gitdir = {
 		interval = 1000,
 		follow_files = true,
 	},
 	attach_to_untracked = true,
+
 	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
+		delay = 300,
 		ignore_whitespace = false,
 	},
 	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-	sign_priority = 6,
+
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
