e580c07d5bc0a28c719c69fdb38cc7d49581cc8d
Imp1ication <1mplicationxwzi@gmail.com>
2024-03-06 19:12:38 +0800

    rebuild impvim

--- init.lua
+++ init.lua
@@ -1,24 +1,2 @@
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.treesitter")
-require("user.autopairs")
-require("user.telescope")
-require("user.gitsigns")
-require("user.toggleterm")
-
-require("view.colorscheme")
-require("view.nvim-tree")
-require("view.bufferline")
-require("view.lualine")
-
-require("lsp.cmp")
-require("lsp.mason")
-require("lsp.lspconfig")
-require("lsp.lspsaga")
-require("lsp.null-ls")
-require("user.comment")
-
-require("user.copilot")
--- require("user.chatgpt")
+require("imp/options")
 
---
+++ lua/imp/options.lua
@@ -0,0 +1,52 @@
+-- basic --
+vim.opt.fileencoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus" -- allows nvim to access the os clipboard
+vim.opt.mouse = "a" -- enabled mouse, "a" for all modes, and "niv" for normal, insert, visual mode
+vim.opt.timeoutlen = 500 -- time in milliseconds to wait for a mapped sequence to complete
+vim.opt.virtualedit = ""
+vim.opt.iskeyword:append("-") -- consider string-string as whole word
+
+-- backup --
+vim.opt.swapfile = false
+vim.opt.undofile = true -- enable persistent undo
+
+-- line number and line column --
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
+vim.opt.signcolumn = "auto"
+
+-- tab and indentation --
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true -- expand tab to spaces
+vim.opt.autoindent = true -- copy indent from current line when starting a new line
+vim.opt.smartindent = true -- make above smarter
+
+-- wrap and scrolloff --
+vim.opt.wrap = true -- line wrapping
+vim.opt.scrolloff = 5 -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 10
+
+-- search --
+vim.opt.hlsearch = false
+vim.opt.incsearch = true -- search while typing a search command
+vim.opt.smartcase = true -- if you type a capital letter, it will be case sensitive
+-- vim.opt.ignorecase = true
+
+-- splitting windows --
+vim.opt.splitright = true -- split vertical widow to the right
+vim.opt.splitbelow = true -- split horizontal widow to the below
+vim.opt.equalalways = false -- don't resize windows when splitting
+
+-- appearance --
+vim.opt.showtabline = 2 -- always show tabline
+vim.opt.cursorline = true
+vim.opt.cmdheight = 1
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.showmode = false -- with lualine, we don't need to show things like -- insert -- anymore
+
+vim.cmd("autocmd bufenter * set formatoptions-=cro") -- disable auto-commenting new lines
+
+vim.g.python3_host_prog = "/bin/python3"
